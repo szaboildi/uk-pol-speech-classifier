49a48db16eb2f4b5e996fb34591d56e9db8fa888
Seoman3 <seoman3@yahoo.de>
2024-05-10 17:58:54 +0200

    Started draft functions in model.py

--- polclassifier/ml_logic/models.py
+++ polclassifier/ml_logic/models.py
@@ -7,6 +7,17 @@ from sklearn.svm import SVC
 
 from polclassifier.params import *
 
+# Timing the TF import
+print(Fore.BLUE + "\nLoading TensorFlow..." + Style.RESET_ALL)
+start = time.perf_counter()
+
+from tensorflow import keras
+from keras import Model, Sequential, layers, regularizers, optimizers
+from keras.callbacks import EarlyStopping
+
+end = time.perf_counter()
+print(f"\n✅ TensorFlow loaded ({round(end - start, 2)}s)")
+
 def randomized_search_model_svm(X, y):
     # Define the hyperparameter grid
     param_grid = {
@@ -50,3 +61,61 @@ def evaluate_model_svm(model, X, y):
     predictions = model.predict(X)
     accuracy = accuracy_score(y, predictions)
     return accuracy
+
+def initialize_model_lstm():
+    """Initialize Recurrent Neural Network with LSTM
+    """
+
+
+
+    print("✅ Model initialized")
+
+    return model
+
+def compile_model_lstm(model: Model, learning_rate=0.0005) -> Model:
+    """
+    Compile the Neural Network
+    """
+
+    optimizer = optimizers.Adam(learning_rate=learning_rate)
+    model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+
+    print("✅ Model compiled")
+
+    return model
+
+def train_model_lstm(
+        model: Model,
+        X: np.ndarray,
+        y: np.ndarray,
+        batch_size=32,
+        patience=15,
+        validation_data=None, # overrides validation_split
+        validation_split=0.3
+    ) -> Tuple[Model, dict]:
+    """
+    Fit the model and return a tuple (fitted_model, history)
+    """
+    print(Fore.BLUE + "\nTraining model..." + Style.RESET_ALL)
+
+    es = EarlyStopping(
+        monitor="val_loss",
+        patience=patience,
+        restore_best_weights=True,
+        verbose=1
+    )
+
+    history = model.fit(
+        X,
+        y,
+        validation_data=validation_data,
+        validation_split=validation_split,
+        epochs=100,
+        batch_size=batch_size,
+        callbacks=[es],
+        verbose=0
+    )
+
+    print(f"✅ Model trained on {len(X)} rows with max accuracy: {round(np.max(history.history['accuracy']), 2)}")
+
+    return model, history
